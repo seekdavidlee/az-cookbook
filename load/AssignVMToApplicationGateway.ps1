8a93bd3b17ba187053f60d291c4160070326448e
David Lee <seekdavidlee@gmail.com>
2020-01-02 20:32:57 -0600

    see if we can add a new ipconfig

--- load/AssignVMToApplicationGateway.ps1
+++ load/AssignVMToApplicationGateway.ps1
@@ -10,6 +10,24 @@ $ApplicationGateway = Get-AzApplicationGateway `
 $nic = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name "${StackName}-nic"
 
 $ApplicationGatewayBackendPoolName = "${ApplicationGatewayStackName}-backendpool"
-Add-AzApplicationGatewayBackendAddressPool -ApplicationGateway $ApplicationGateway `
-    -Name $ApplicationGatewayBackendPoolName `
-    -BackendIPAddresses $nic.IpConfigurations.PrivateIpAddress
+
+$ipConfig = New-Object -TypeName Microsoft.Azure.Commands.Network.Models.PSNetworkInterfaceIPConfiguration
+$ipConfig.Id = $nic.IpConfigurations.Id
+$ipConfig.LoadBalancerBackendAddressPools = @()
+$ipConfig.ApplicationGatewayBackendAddressPools = @()
+$ipConfig.LoadBalancerInboundNatRules = @()
+$ipConfig.ApplicationSecurityGroups = @()
+
+$any = Get-AzApplicationGatewayBackendAddressPool -ApplicationGateway $ApplicationGateway `
+    -Name $ApplicationGatewayBackendPoolName
+
+if ($agw.BackendAddressPools.BackendIpConfigurations -is [array]) {
+    Write-Host "Adding new ip config to existing array"
+    $agw.BackendAddressPools.BackendIpConfigurations += $ipConfig
+} else {
+    Write-Host "Converting property to array and adding new ip config to array property"
+    $agw.BackendAddressPools.BackendIpConfigurations = @($agw.BackendAddressPools.BackendIpConfigurations, $ipConfig)
+}
+
+"Updating application gateway"
+Set-AzApplicationGateway -ApplicationGateway $agw

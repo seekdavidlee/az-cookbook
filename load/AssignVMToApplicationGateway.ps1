def4d3b11a171ccf4bbb23413bc9467fba7d2e88
David Lee <seekdavidlee@gmail.com>
2020-01-02 20:51:01 -0600

    Set from nic side

--- load/AssignVMToApplicationGateway.ps1
+++ load/AssignVMToApplicationGateway.ps1
@@ -10,23 +10,8 @@ $ApplicationGateway = Get-AzApplicationGateway `
 $nic = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name "${StackName}-nic"
 
 $ApplicationGatewayBackendPoolName = "${ApplicationGatewayStackName}-backendpool"
+$backendpool = (Get-AzApplicationGatewayBackendAddressPool -Name $ApplicationGatewayBackendPoolName -ApplicationGateway $ApplicationGateway)
 
-$ipConfig = New-Object -TypeName Microsoft.Azure.Commands.Network.Models.PSNetworkInterfaceIPConfiguration
-$ipConfig.Id = $nic.IpConfigurations.Id
-$ipConfig.LoadBalancerBackendAddressPools = @()
-$ipConfig.ApplicationGatewayBackendAddressPools = @()
-$ipConfig.LoadBalancerInboundNatRules = @()
-$ipConfig.ApplicationSecurityGroups = @()
-
-$ApplicationGateway.BackendAddressPools
-
-if ($ApplicationGateway.BackendAddressPools.BackendIpConfigurations -is [array]) {
-    Write-Host "Adding new ip config to existing array"
-    $ApplicationGateway.BackendAddressPools.BackendIpConfigurations += $ipConfig
-} else {
-    Write-Host "Converting property to array and adding new ip config to array property"
-    $ApplicationGateway.BackendAddressPools.BackendIpConfigurations = @($ApplicationGateway.BackendAddressPools.BackendIpConfigurations, $ipConfig)
-}
-
-"Updating application gateway"
-Set-AzApplicationGateway -ApplicationGateway $ApplicationGateway
+# https://github.com/Azure/azure-rest-api-specs/issues/7475
+$nic.IpConfigurations[0].ApplicationGatewayBackendAddressPools += $backendpool
+$nic | Set-AzNetworkInterface

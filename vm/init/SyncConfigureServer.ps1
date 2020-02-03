d0638b1aca49216ffbefdf6ddcd954e4cc4debd6
David Lee <seekdavidlee@gmail.com>
2020-02-02 23:27:44 -0600

    debug

--- vm/init/SyncConfigureServer.ps1
+++ vm/init/SyncConfigureServer.ps1
@@ -9,16 +9,25 @@ $storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupN
 $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageAccountKey
 
 # Temporary add Cloud Shell ip to allow list.
-$ip = (curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+$ip = (Invoke-RestMethod -Uri 'https://api.ipify.org?format=json').ip
 
 Add-AzStorageAccountNetworkRule -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName -IPAddressOrRange $ip
 
-Start-Sleep -Seconds 15
+for($i = 0; $i -lt 5; $i++) {
 
-Set-AzStorageBlobContent -Container "deploy" `
+    Set-AzStorageBlobContent -Container "deploy" `
     -File "$RootDirectory/vm/init/ConfigureServer.ps1" -Blob "ConfigureServer.ps1" `
     -Context $ctx `
     -Force
+    
+    if ($Error -and $Error[$Error.Count - 1].ToString().Contains("HTTP Status Code: 403")) {
+        Write-Host "Retry $i"
+        Start-Sleep -Seconds 3
+    } else {
+        Write-Host "Done!"
+        break
+    }
+}
 
 # Remove Cloud Shell ip from allow list since we are done.
 Remove-AzStorageAccountNetworkRule -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName -IPAddressOrRange $ip

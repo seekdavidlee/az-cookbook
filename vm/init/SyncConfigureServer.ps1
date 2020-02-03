35e714b91664dabeab583ddf22c06395bbe3e496
David Lee <seekdavidlee@gmail.com>
2020-02-02 18:13:32 -0600

    Sync ConfigureServer.ps1

--- vm/init/SyncConfigureServer.ps1
+++ vm/init/SyncConfigureServer.ps1
@@ -1,2 +1,12 @@
+param(
+    $ResourceGroupName, 
+    $StorageAccountName)
+
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName `
+    -Name $StorageAccountName).Value[0]
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageAccountKey
+
 Set-AzureStorageBlobContent -Container "deploy" `
-    -File ".\ConfigureServer.ps1" -Blob "ConfigureServer.ps1"
+    -File ".\ConfigureServer.ps1" -Blob "ConfigureServer.ps1" `
+    -Context $ctx

a7973476d4d9c622d87218a27d0251920aaf8bef
ENG-KEONG DAVID LEE <36802330+seekdavidlee@users.noreply.github.com>
2020-02-03 20:04:57 -0600

    Update automation runbook (#8)

--- vm/init/ConfigureServer.ps1
+++ vm/init/ConfigureServer.ps1
@@ -1,26 +1,62 @@
 param([switch] $Web, [switch] $DotNetCore, [switch] $Git)
 
+$logFileName = "$(get-date -f yyyy-MM-dd-HHmmss).log"
+function LogMessage {
+    param (
+        $LogFileName,
+        $Message
+    )
+    if (!(Test-Path "C:\logs")) {
+        New-Item -Path C:\logs -ItemType Directory
+    } 
+    $Message = $(get-date -f yyyy-MM-dd-HHmmss) + " " + $Message
+
+    $path = "C:\logs\$LogFileName"
+    if (!(Test-Path $path)) {
+        
+        Set-Content -Path $path -Value $Message
+    } else {
+        Add-Content -Path $path -Value $Message
+    }
+}
+
+LogMessage -Message "Installing components..." -LogFileName $logFileName
+
 if ($Web) {
+    LogMessage -Message "Installing web server" -LogFileName $logFileName
     Add-WindowsFeature Web-Server
     Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "<html><head><title>Hello World</title><body><b>Hello World from $($env:computername)!</b></body></html>"
+    LogMessage -Message "Done installing web server" -LogFileName $logFileName
 }
 
 if ($DotNetCore) {
-    if (!Test-Path "C:\tools") {
+
+    LogMessage -Message "Installing dotnet core" -LogFileName $logFileName
+    if (!(Test-Path "C:\tools")) {
         New-Item -Path C:\tools -ItemType Directory
     }
-    Invoke-WebRequest -Uri https://dot.net/v1/dotnet-install.ps1 -UseBasicParsing -OutFile C:\tools\dotnet-install.ps1
+    
+    Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/854ca330-4414-4141-9be8-5da3c4be8d04/3792eafd60099b3050313f2edfd31805/dotnet-sdk-3.1.101-win-x64.exe -UseBasicParsing -OutFile C:\tools\dotnet-sdk-3.1.101-win-x64.exe
     Push-Location C:\tools
-    .\dotnet-install.ps1 -Channel LTS
+
+    .\dotnet-sdk-3.1.101-win-x64.exe /install /norestart /quiet /log "C:\tools\dotnet-sdk-3.1.101-win-x64.log"
     Pop-Location
+    LogMessage -Message "Done installing dotnet core" -LogFileName $logFileName
+
+    # Display dotnet version
+    LogMessage -Message (dotnet --version) -LogFileName $logFileName
 }
 
 if ($Git) {
-    if (!Test-Path "C:\tools") {
+
+    LogMessage -Message "Installing git" -LogFileName $logFileName
+    if (!(Test-Path "C:\tools")) {
         New-Item -Path C:\tools -ItemType Directory
     }
     Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.25.0.windows.1/Git-2.25.0-64-bit.exe -OutFile C:\tools\Git-2.25.0-64-bit.exe
     Push-Location C:\tools
     .\Git-2.25.0-64-bit.exe /SILENT
     Pop-Location
+
+    LogMessage -Message "Done installing git" -LogFileName $logFileName
 }
---
+++ vm/init/SyncConfigureServer.ps1
@@ -0,0 +1,38 @@
+param(
+    $RootDirectory,
+    $ResourceGroupName, 
+    $StorageAccountName)
+
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName `
+    -Name $StorageAccountName).Value[0]
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageAccountKey
+
+# Temporary add Cloud Shell ip to allow list.
+$ip = (Invoke-RestMethod -Uri 'https://api.ipify.org?format=json').ip
+
+Add-AzStorageAccountNetworkRule -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName -IPAddressOrRange $ip
+
+$lastErrorCount = $Error.Count
+for($i = 0; $i -lt 5; $i++) {
+
+    Set-AzStorageBlobContent -Container "deploy" `
+    -File "$RootDirectory/vm/init/ConfigureServer.ps1" -Blob "ConfigureServer.ps1" `
+    -Context $ctx `
+    -Force
+    
+    if ($Error.Count -gt $lastErrorCount -and $Error[$Error.Count - 1].ToString().Contains("HTTP Status Code: 403")) {
+        $lastErrorCount = $Error.Count
+        Write-Host "Retry $i"
+        # Increment wait by additional seconds
+        $wait = $i + 3
+        Write-Host "Waiting $wait seconds"
+        Start-Sleep -Seconds $wait
+    } else {
+        Write-Host "Done!"
+        break
+    }
+}
+
+# Remove Cloud Shell ip from allow list since we are done.
+Remove-AzStorageAccountNetworkRule -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName -IPAddressOrRange $ip

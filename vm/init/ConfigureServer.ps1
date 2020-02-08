f8629f65ca859fc8124b8f54414e437a51e7fdfe
ENG-KEONG DAVID LEE <36802330+seekdavidlee@users.noreply.github.com>
2020-02-08 08:20:10 -0600

    initial checkin for installing ssh (#11)

--- vm/init/ConfigureServer.ps1
+++ vm/init/ConfigureServer.ps1
@@ -60,3 +60,20 @@ if ($Git) {
 
     LogMessage -Message "Done installing git" -LogFileName $logFileName
 }
+
+LogMessage -Message "Installing OpenSSH Server" -LogFileName $logFileName
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+Start-Service sshd
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+# Confirm the Firewall rule is configured. It should be created automatically by setup. 
+Get-NetFirewallRule -Name *ssh*
+# There should be a firewall rule named "OpenSSH-Server-In-TCP", which should be enabled
+# If the firewall does not exist, create one
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+LogMessage -Message "Done installing OpenSSH Server" -LogFileName $logFileName
+
+LogMessage -Message "Setting up powershell as the default for SSH" -LogFileName $logFileName
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+LogMessage -Message "Done setting up powershell as the default for SSH" -LogFileName $logFileName
